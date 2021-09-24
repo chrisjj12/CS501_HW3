7ff150ac9913e6c4835db4fce5d3e0f7a60233a1
Andy Vo <andyvo@bu.edu>
2021-09-23 20:13:52 -0400

    flash cards xml

---
+++ Flash Card App/app/src/main/java/com/example/flashcardapp/Flashcards.java
@@ -0,0 +1,14 @@
+package com.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Flashcards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcards);
+    }
+}
