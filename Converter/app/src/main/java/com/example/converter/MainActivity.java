fc5dbc05a82c24f9c77a93393ec4bbb99ad72926
Anne Zhang <annezhang@Annes-MacBook-Pro.local>
2021-09-27 01:34:59 -0400

    converter done

--- Converter/app/src/main/java/com/example/converter/MainActivity.java
+++ Converter/app/src/main/java/com/example/converter/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTempF;
     private int newtempC;
     private int newtempF;
+    private TextView msg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         textViewF = (TextView)findViewById(R.id.textViewF);
         textViewTempF = (TextView)findViewById(R.id.textViewTempF);
 
+        msg = (TextView)findViewById(R.id.textViewIntMsg);
+
         seekBarC.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
             @Override
@@ -40,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 newtempF = (int) (Integer.valueOf(String.valueOf(tempC))*1.8 + 32);
                 textViewTempC.setText(String.valueOf(tempC));
                 textViewTempF.setText(String.valueOf(newtempF));
+                if(tempC < 12){
+                    msg.setText("Time to go skiing!");
+                }
+                else{
+                    msg.setText("Time to go swimming!");
+                }
 
 
             }
@@ -61,6 +71,12 @@ public class MainActivity extends AppCompatActivity {
                 newtempC = (int) ((Integer.valueOf(String.valueOf(tempF))-32) * .5556);
                 textViewTempF.setText(String.valueOf(tempF));
                 textViewTempC.setText(String.valueOf(newtempC));
+                if(tempF < 53){
+                    msg.setText("Time to go skiing!");
+                }
+                else{
+                    msg.setText("Time to go swimming!");
+                }
 
             }
 
